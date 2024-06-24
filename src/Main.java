74d541408de0efe8af8038113b64f3afcbf164e1
Andrey Cortez <70648908+AndreyCortez@users.noreply.github.com>
2024-06-24 15:45:58 -0300

    acaba pelo amor de Deus

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.SwingUtilities;
 
 // Andrey Cortez Rufino - 11819487
 // Francyélio de Jesus Campos Lima - 13676537
+// Catarina Moreira Lima - 8957221
 
 // Classe main, serve apenas para inicializar a interface gráfica, mas poderia
 // servir outros propósitos caso o código continuasse a ser desenvolvido
